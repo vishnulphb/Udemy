5ed06f32a75eb6624632207fbc259ba51f5f7ee8
Vishnu Pillai (ADE) <vpillai@ade.arkgov.net>
2016-06-13 12:30:26 -0500

    Initial Commit + Insertion Sort

---
+++ Udemy-DataStructures/Sorting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_DataStructures
+{
+    class Sorting
+    {
+        public static void InsertionSort()
+        {
+            Console.WriteLine("Enter the numbers for insertion sort separated by commas:");
+            string numbers = Console.ReadLine();
+            int[] nums = Array.ConvertAll(numbers.Split(','), Int32.Parse);
+
+            for (int i = 0; i <= nums.Length-1; i++)
+            {
+                int j = i - 1;
+                int current = nums[i];
+
+                while (j >= 0 && nums[j] > current)
+                {
+                    nums[j + 1] = nums[j];
+                    j = j - 1;
+                }
+                nums[j + 1] = current;
+            }
+
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
